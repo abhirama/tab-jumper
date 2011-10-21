daab9844476ba45a77d5dfc24dba2f5a642f9c8f
abhat@JT2YBQ1-ABHAT.corp.zynga.com <abhat@JT2YBQ1-ABHAT.corp.zynga.com>
2011-10-21 21:24:43 +0530

    First cut.

---
+++ inject.js
@@ -0,0 +1,28 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//key code p -> 80
+//key code 0 -> 79
+document.addEventListener("keydown", function(event){
+    if (event.keyCode == 80) {
+      // Send message to background page to toggle tab
+      console.log("Sending tab changing request");
+      chrome.extension.sendRequest({toggle_tabs: true, href: document.location.href}, function(response) {
+        // Do stuff on successful response
+      });
+    }
+
+    if (event.keyCode == 79) {
+      // Send message to background page to toggle tab
+      console.log("Sending tab naming request");
+      chrome.extension.sendRequest({name_tab: true}, function(response) {
+        // Do stuff on successful response
+      });
+      /*
+      chrome.tabs.getCurrent(function(tab){
+        tab.tabJumperName = "foo";  
+      });
+      */
+    }
+
+}, false);
