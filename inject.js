cb202bda2fc1ad9747a37b743768adcff75dcf9f
abhat@JT2YBQ1-ABHAT.corp.zynga.com <abhat@JT2YBQ1-ABHAT.corp.zynga.com>
2011-10-25 23:53:35 +0530

    Polishing.

--- inject.js
+++ inject.js
@@ -4,11 +4,15 @@ var N_KEY_CODE = 78;
 document.addEventListener("keydown", function(event){
     if (event.altKey && event.keyCode == J_KEY_CODE) {
       var tabNameLoc = prompt("Please enter the name of the tab you want to open");
-      chrome.extension.sendRequest({jump_tabs: true, tabName: tabNameLoc});
+      if (tabNameLoc) {
+        chrome.extension.sendRequest({jump_tabs: true, tabName: tabNameLoc});
+      }
     }
 
     if (event.altKey && event.keyCode == N_KEY_CODE) {
       var tabNameLoc = prompt("Please enter a name for your tab");
-      chrome.extension.sendRequest({name_tab: true, tabName: tabNameLoc});
+      if (tabNameLoc) {
+        chrome.extension.sendRequest({name_tab: true, tabName: tabNameLoc});
+      }
     }
 }, false);
