cb0606b08e94abd958b0bc64ca735be788024c42
abhat@JT2YBQ1-ABHAT.corp.zynga.com <abhat@JT2YBQ1-ABHAT.corp.zynga.com>
2011-10-30 14:38:23 +0530

    Changed text.

--- inject.js
+++ inject.js
@@ -3,14 +3,14 @@ var N_KEY_CODE = 78;
 
 document.addEventListener("keydown", function(event){
     if (event.altKey && event.keyCode == J_KEY_CODE) {
-      var tabNameLoc = prompt("Please enter the name of the tab you want to open");
+      var tabNameLoc = prompt("Jump to");
       if (tabNameLoc) {
         chrome.extension.sendRequest({jump_tabs: true, tabName: tabNameLoc});
       }
     }
 
     if (event.altKey && event.keyCode == N_KEY_CODE) {
-      var tabNameLoc = prompt("Please enter a name for your tab");
+      var tabNameLoc = prompt("Name your tab");
       if (tabNameLoc) {
         chrome.extension.sendRequest({name_tab: true, tabName: tabNameLoc});
       }
